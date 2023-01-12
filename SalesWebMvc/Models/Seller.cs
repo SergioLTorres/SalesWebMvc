247ad3810559bf8417f5caaaca26636171446f06
SergioLTorres <sergiotorres0564@gmail.com>
2023-01-12 08:07:29 -0300

    Validation

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -9,17 +9,24 @@ namespace SalesWebMvc.Models
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "{0} é Obrigatório")]
+        [StringLength(60,MinimumLength = 3,ErrorMessage = "{0} tem que ter entre {2} e {1} caracteres")]
         [Display(Name = "Nome")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "{0} é Obrigatório")]
+        [EmailAddress(ErrorMessage = "Entre com um e-mail válido")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "{0} é Obrigatório")]
         [Display(Name = "Data de Nascimento")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
-        
+
+        [Required(ErrorMessage = "{0} é Obrigatório")]
+        [Range(100.0, 50000.0, ErrorMessage = "{0} must be from {1} to {2}")]
         [Display(Name = "Salário Base")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
